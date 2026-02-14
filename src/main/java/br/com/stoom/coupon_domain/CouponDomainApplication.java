0895c83c61d784647cb113ee3ff5dacf6917edd7
willianbaldez <wnbaldez@gmail.com>
2026-02-14 13:43:22 -0300

    chore: iniciando projeto com spring initializr

---
+++ src/main/java/br/com/stoom/coupon_domain/CouponDomainApplication.java
@@ -0,0 +1,13 @@
+package br.com.stoom.coupon_domain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouponDomainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CouponDomainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/stoom/coupon_domain/CouponDomainApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.stoom.coupon_domain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponDomainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
